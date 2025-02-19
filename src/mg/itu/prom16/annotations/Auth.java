158a085cce6d8c1fad54b4636c82235cd2a330e5
kennyrts <ratsimbazafykenny@gmail.com>
2025-02-19 05:34:47 +0300

    sprint16-2608

--- src/mg/itu/prom16/annotations/Auth.java
+++ src/mg/itu/prom16/annotations/Auth.java
@@ -6,7 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 // Define the Auth annotation
-@Target(ElementType.METHOD) // The annotation can be applied to methods
+@Target({ElementType.METHOD, ElementType.TYPE}) // The annotation can be applied to methods and classes
 @Retention(RetentionPolicy.RUNTIME) // The annotation is available at runtime
 public @interface Auth {
     String value(); // The required role
